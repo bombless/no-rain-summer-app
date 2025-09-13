2fcda2e69da5d7347ca105ea39002a19b9b05989
York Xiang <bombless@126.com>
2025-09-14 00:11:08 +0800

    修复界面

--- src/App.vue
+++ src/App.vue
@@ -10,9 +10,8 @@ function get(url) {
 </script>
 
 <template>
-  <main class="container">
 <div id="content">
-  <div id="map-container"></div>
+  <div id="map_container"></div>
       <div id="pannel">
         <fieldset>
           <input type="radio" name="temperature-type" id="higherTemperature"></input>
@@ -31,7 +30,6 @@ function get(url) {
       </div>
     </div>
 
-  </main>
 </template>
 
 
@@ -44,7 +42,7 @@ function get(url) {
       body { margin: 0; }
       #content { position: relative; }
       window, #content { width: 1500px; height: 900px; }
-      #container { position: absolute; left: 0; width: 1200px; height: 900px; }
+      #map_container { position: absolute; left: 0; width: 1200px; height: 900px; }
       #pannel { position: absolute; left: 1200px; height: 900px; }
       .pannel { overflow-y: scroll; }
       #pannel li { cursor: pointer; }
@@ -68,98 +66,5 @@ function get(url) {
   -webkit-text-size-adjust: 100%;
 }
 
-.container {
-  margin: 0;
-  padding-top: 10vh;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  text-align: center;
-}
-
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: 0.75s;
-}
-
-.logo.tauri:hover {
-  filter: drop-shadow(0 0 2em #24c8db);
-}
-
-.row {
-  display: flex;
-  justify-content: center;
-}
-
-a {
-  font-weight: 500;
-  color: #646cff;
-  text-decoration: inherit;
-}
-
-a:hover {
-  color: #535bf2;
-}
-
-h1 {
-  text-align: center;
-}
-
-input,
-button {
-  border-radius: 8px;
-  border: 1px solid transparent;
-  padding: 0.6em 1.2em;
-  font-size: 1em;
-  font-weight: 500;
-  font-family: inherit;
-  color: #0f0f0f;
-  background-color: #ffffff;
-  transition: border-color 0.25s;
-  box-shadow: 0 2px 2px rgba(0, 0, 0, 0.2);
-}
-
-button {
-  cursor: pointer;
-}
-
-button:hover {
-  border-color: #396cd8;
-}
-button:active {
-  border-color: #396cd8;
-  background-color: #e8e8e8;
-}
-
-input,
-button {
-  outline: none;
-}
-
-#greet-input {
-  margin-right: 5px;
-}
-
-@media (prefers-color-scheme: dark) {
-  :root {
-    color: #f6f6f6;
-    background-color: #2f2f2f;
-  }
-
-  a:hover {
-    color: #24c8db;
-  }
-
-  input,
-  button {
-    color: #ffffff;
-    background-color: #0f0f0f98;
-  }
-  button:active {
-    background-color: #0f0f0f69;
-  }
-}
 
 </style>
--- src/renderer.js
+++ src/renderer.js
@@ -2,6 +2,22 @@
     import { Cities } from './cities';
     import debounce from 'debounce';
 
+import { invoke } from "@tauri-apps/api/core";
+
+
+
+function log() {
+  let msg = '';
+  for (let i = 0; i < arguments.length; i += 1) {
+    if (undefined === arguments[i]) {
+        return invoke('log_string', { msg: 'logging undefined: argument ' + i });
+    }
+    if (msg) msg += "\n";
+    msg += arguments[i].toString();
+  }
+  return invoke('log_string', { msg });
+};
+
     AMapLoader.load({
         "key": "bd025ef2a45752cd896864d70447a76f",          // 申请好的Web端开发者Key，首次调用 load 时必填
         "version": "2.0",   // 指定要加载的 JSAPI 的版本，缺省时默认为 1.4.15
@@ -14,7 +30,7 @@
         "version": '2.0'  // Loca 版本
         },
     }).then((AMap)=>{
-        const map = new AMap.Map('map-container');
+        const map = new AMap.Map('map_container');
         init(AMap, map);
         //map.addControl(new AMap.Scale());
     }).catch((e)=>{
@@ -25,7 +41,8 @@
             icon: 'http://vdata.amap.com/icons/b18/1/2.png',
             position: info.position,   // 经纬度对象，也可以是经纬度构成的一维数组[116.39, 39.9]
             title: info.province + info.city,
-        });
+        });;
+        log('position', info.position, 'title', info.province + info.city);
 
         // 将创建的点标记添加到已有的地图实例：
         debounce(() => { map.add(marker); map.setFitView(); }, 20).call();
@@ -54,7 +71,7 @@
         });
 
 
-        const cities = new Cities(x => map.setFitView(x), info => mark(map, info));
+        const cities = new Cities(x => {/*map.setFitView(x)*/}, info => mark(map, info));
         for (const item of await cities.items) {
 
             const info = document.createElement('div');
